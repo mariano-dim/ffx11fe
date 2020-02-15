a33a308f0e63cf9010e932b448c0e895fa65b306
Mariano Di Maggio <mariano.dim@gmail.com>
2020-02-15 19:16:47 -0300

    add validations

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -18,7 +18,15 @@ export class ClienteService {
 
   getClientes(): Observable<Cliente[]>{
   //  return of(CLIENTES);
-      return this.http.get<Cliente[]>(this.urlEndpoint);
+      return this.http.get(this.urlEndpoint).pipe(
+        map(response =>{
+          let clientes = response as Cliente[];
+          return clientes.map(cliente =>{
+            cliente.nombre = cliente.nombre.toUpperCase();
+            return cliente;
+          })
+        })
+      );
   }
 
 
@@ -37,6 +45,10 @@ export class ClienteService {
     return this.http.post(this.urlEndpoint, cliente, {headers: this.httpHeaders}).pipe(
       map( (response: any) =>  response.cliente as Cliente),
       catchError (e => {
+
+        if(e.status==400){
+          return throwError(e);
+        }
         console.error(e.error.mensaje);
         swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
@@ -47,6 +59,10 @@ export class ClienteService {
   update(cliente: Cliente): Observable<any>{
     return this.http.put<any>(`${this.urlEndpoint}/${cliente.id}`, cliente, { headers : this.httpHeaders}).pipe(
       catchError (e => {
+
+        if(e.status==400){
+          return throwError(e);
+        }
         console.error(e.error.mensaje);
         swal.fire(e.error.mensaje, e.error.error, 'error');
         return throwError(e);
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -12,6 +12,7 @@ export class FormComponent implements OnInit {
 
   private cliente: Cliente = new Cliente();
   private titulo: string = "Crear Cliente";
+  private errores: string[];
 
   constructor(private clienteService: ClienteService,
               private router: Router,
@@ -39,6 +40,11 @@ export class FormComponent implements OnInit {
       .subscribe( cliente => {
         this.router.navigate(['/clientes'])
         swal.fire('Nuevo Cliente', `${cliente.nombre} ha sido creado con exito`, 'success')
+      },
+      err =>{
+        this.errores = err.error.errors as string[];
+        console.error('Codigo de error desde el Backend ' + err.status);
+        console.error(err.error.errors);
       }
     )
   }
@@ -49,6 +55,11 @@ export class FormComponent implements OnInit {
       .subscribe( json =>{
         this.router.navigate(['/clientes'])
         swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}`, 'success')
+      },
+      err =>{
+        this.errores = err.error.errors as string[];
+        console.error('Codigo de error desde el Backend ' + err.status);
+        console.error(err.error.errors);
       }
     )
   }
